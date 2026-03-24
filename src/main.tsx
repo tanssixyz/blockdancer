2712457555ff02cc175962a091d320cbdeb89609
HappyDancer <admin@improtango.fi>
2026-03-25 01:14:15 +0200

    fix vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./index.css"
 import "./App.css"
 import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Analytics />
     <App />
+    <Analytics />
   </StrictMode>
 )
