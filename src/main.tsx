94cd377095b92b9ad901181263ec0af11841b6c7
tanssixyz <happydancer@tanssi.xyz>
2026-03-25 01:07:38 +0200

    Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Analytics } from "@vercel/analytics/next"
 import "./index.css"
 import "./App.css"
 import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>
 )
